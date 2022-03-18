938639143b06f5725afd01665e15623c4bac18b4
Wuraola17 <titilayoojeyode@gmail.com>
2022-03-18 07:39:37 -0700

    the root commit for task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,28 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include "stdio.h"
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-#endif
+	return (largest);
+}
