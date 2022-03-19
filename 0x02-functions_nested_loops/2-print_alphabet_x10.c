190ed8bcaf1632ac64d82f07428f100c5bff76f4
Wuraola17 <titilayoojeyode@gmail.com>
2022-03-19 05:15:05 -0700

    Another commit for task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,27 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10  - A function that prints the alphabets 10 times
+ * print_alphabet_x10 - print alphabet
  *
- * Description: Funtion that prints the alphabets
- * 10 times and a newline after each
-*/
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
-	int i, j;
+int alphabet;
+int count;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
+
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+
 }
