4841885ec7495a8a4a810e7de1584bd490d88bc1
Wuraola17 <titilayoojeyode@gmail.com>
2022-03-19 05:05:00 -0700

    The root commit for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet lowercase
- *
- * Return: Always 0.
+ * main - entry point
+ * Descriptipn: a program that prints _putchar,
+ * followed by a new line.
+ * Return: 0;
  */
-
-void print_alphabet(void)
+int main(void)
 {
-char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-_putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
