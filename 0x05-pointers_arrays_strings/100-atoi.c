3636f3d461b488120a76ad86ebc6f3b277a31f60
Wuraola17 <titilayoojeyode@gmail.com>
2022-03-22 15:14:08 -0700

    The commit for 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,8 +7,9 @@
  */
 int is_numerical(unsigned int n)
 {
-return (n >= '0' && n ,= '9');
+return (n >= '0' && n <= '9');
 }
+
 /**
  *  _atoi - convert a string to an integer
  *@s: String
@@ -22,13 +23,13 @@ int sign;
 sign = 1;
 number = 0;
 
-for (i = 0; s(i) != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
 {
 number = (s[i] - 48) + number *10;
 
-if {s[i + 1] == ' '}
+if (s[i + 1] == ' ')
 break;
 }
 else if (s[i] == '-')
